b76fe695431166f3ffc99f1056ab6950c214b7bd
Simon <simon.gurung@thimphutechpark.bt>
2022-12-20 15:45:43 +0600

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FrontEndService.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
